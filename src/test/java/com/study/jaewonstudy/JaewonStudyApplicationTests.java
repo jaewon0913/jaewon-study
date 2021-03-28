7fb88a9f2adcaa5c6198c19ca5d37631cce7a919
jaewon0913 <jaewon9937@gmail.com>
2021-03-28 14:06:09 +0900

    프로젝트 생성

---
+++ src/test/java/com/study/jaewonstudy/JaewonStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jaewonstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaewonStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
