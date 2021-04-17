e245d59ec8036c6b0fefef1594f0edccf5201dac
jaewon0913 <jaewon9937@gmail.com>
2021-04-17 21:03:45 +0900

    travis ci 연동 테스트

--- src/main/java/com/study/jaewonstudy/webservice/web/WebController.java
+++ src/main/java/com/study/jaewonstudy/webservice/web/WebController.java
@@ -1,15 +1,13 @@
 package com.study.jaewonstudy.webservice.web;
 
-import com.study.jaewonstudy.webservice.web.jpa.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
-    
+
     @GetMapping("/")
     public String index(){
 
