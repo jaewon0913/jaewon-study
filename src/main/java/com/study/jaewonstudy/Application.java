b3542c605f53ac4d6e53064ded7cc87a74a1ac12
jaewon0913 <jaewon9937@gmail.com>
2021-04-18 01:11:40 +0900

    외부설정파일 가지고 와서 실행되도록 수정

--- src/main/java/com/study/jaewonstudy/Application.java
+++ src/main/java/com/study/jaewonstudy/Application.java
@@ -2,14 +2,21 @@ package com.study.jaewonstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/home/ec2-user/app/dataSource.properties";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
